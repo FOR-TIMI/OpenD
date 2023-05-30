920183a3e1db8a0c17c59f049ae719332ee4436f
FOR-TIMI <timiplayskeys@gmail.com>
2023-05-29 23:21:25 -0400

    Added Item component

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import homeImage from "../../assets/home-img.png";
+import Footer from "./Footer";
+import Header from "./Header";
+import Item from "./Item";
 
 function App() {
+  const nftId = "rrkah-fqaaa-aaaaa-aaaaq-cai";
+
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      {/* <img className="bottom-space" src={homeImage} /> */}
+      <Item id={nftId} />
       <Footer />
     </div>
   );
--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { Principal } from "@dfinity/principal";
+import React, { useEffect, useState } from "react";
+import { idlFactory } from "../../../declarations/nft/index";
 import logo from "../../assets/logo.png";
 
-function Item() {
+function Item({ id }) {
+  const [name, setName] = useState(null);
+  const [owner, setOwner] = useState(null);
+  const [content, setContent] = useState(null);
+
+  const url = "http://127.0.0.1:8000/";
+  const agent = new HttpAgent({ host: url });
+
+  const loadNft = async () => {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: Principal.fromText(id),
+    });
+
+    const name = await NFTActor.getName();
+    const owner = await NFTActor.getOwner();
+    const contentUnit8Array = await NFTActor.getContent();
+
+    const imageContent = new Uint8Array(contentUnit8Array);
+
+    const image = URL.createObjectURL(
+      new Blob([imageContent.buffer], { type: "image/png" })
+    );
+
+    setName(name);
+    setOwner(owner.toText());
+    setContent(image);
+  };
+
+  useEffect(() => {
+    loadNft();
+  }, []);
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={content}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {owner}
           </p>
         </div>
       </div>
